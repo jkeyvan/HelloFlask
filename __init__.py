71c7b7358cd5c20cc203271509a20d1a06f45df7
keyvan jamshidi jam <j.keyvan@gmail.com>
2016-05-20 12:32:09 +0430

    second commit
    commit of init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash
+    abort, render_template, flash,make_response
 from contextlib import closing
 
 DATABASE = 'flaskr.db'
@@ -60,8 +60,34 @@ def add_entry():
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
 
+
+    if 'usetname' in session:
+        status='your are login'
+    else:status='your are not login , please sign in '
+
+    return render_template('show_entries.html', entries=entries,status=status)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+<form action="" method="post">
+<p><input type=text name=username>
+<p><input type=submit value=Login>
+</form>
+'''
+@app.route('/logout')
+def logout():
+# remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+# set the secret key. keep this really secret:
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -85,7 +111,11 @@ def login(name):
 @app.route('/user/<username>')
 def profile(username): pass
 
-
+@app.route('/lo')
+def func():
+    resp = make_response(render_template('home.html'))
+    resp.set_cookie('username', 'the username')
+    return resp
 if __name__ == '__main__':
     app.debug=True
     app.run()
